ca84df04d065d74af4ac4a787c0ff7413f18b837
Aaryan Kothari <53724307+aaryankotharii@users.noreply.github.com>
2020-11-10 00:02:10 +0530

    add Leaf 🍂 (#1)

---
+++ CupcakeCorner/Sources/App/routes.swift
@@ -0,0 +1,21 @@
+import Vapor
+import Leaf
+import FluentSQLiteDriver
+
+func routes(_ app: Application) throws {
+    
+    app.get {  req -> EventLoopFuture<View> in
+        return Cupcake.query(on: req.db).all().flatMap { cupcakes -> EventLoopFuture<View> in
+            return req.leaf.render("home", ["cupcakes":cupcakes])
+        }
+    }
+    
+    app.post("add") { req -> EventLoopFuture<Response> in
+        let cupcake = try req.content.decode(Cupcake.self)
+        return cupcake.create(on: req.db).map { cupcake in
+            return req.redirect(to: "/")
+        }
+    }
+}
+
+
