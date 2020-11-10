9cb01fdf2c628e7f331173271aad0f8198f1616f
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-11-10 18:36:00 +0530

    add iOS

---
+++ CupcakeCorner-iOS/CupcakeCorner-iOS/CupcakeCorner_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_iOSApp.swift
+//  CupcakeCorner-iOS
+//
+//  Created by Aaryan Kothari on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
